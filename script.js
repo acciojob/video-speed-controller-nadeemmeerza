348dc85c506295a8dc100d040354688442eac3fb
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-19 11:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const inputs = document.querySelectorAll('.controls input');
 
     function handleUpdate() {
+	  
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+    // inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    // inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+let isplaying = false
+function play(){
+	const vid = document.getElementById("vid");
+	
+	isplaying = !isplaying;
+	if(isplaying)
+		{document.getElementById("play-pause").value= '\u23f8'
+		vid.play();
+		}
+	else
+		{document.getElementById("play-pause").value= '\u23f5'
+		vid.pause();
+		}
+	
+}
